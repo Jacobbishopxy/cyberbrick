6de3c3f0087df30b384e8a781a5615162d20558f
Jacob Xie <jacobbishopxy@gmail.com>
2020-11-13 10:57:41 +0800

    MNT: update docker

--- docker-app/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# author: Jacob Bishop
-
-source ../resources/docker.env
-docker restart ${CONTAINER_NAME}
--- docker/docker-node/setup.sh
+++ docker/docker-node/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # author: Jacob Bishop
 
-source ../resources/docker.env
+source ../../resources/docker.env
 docker pull "${NODE_IMAGE}":"${NODE_IMAGE_VERSION}"
 docker image tag \
   "${NODE_IMAGE}":"${NODE_IMAGE_VERSION}" "${FUND_IMAGE}":"${FUND_IMAGE_VERSION}"
