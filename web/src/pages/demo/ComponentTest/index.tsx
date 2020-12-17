e0e9ba08ecd850353c78b06debf1089e09c9724b
Jacob Xie <jacobbishopxy@gmail.com>
2020-12-17 11:39:18 +0800

    MNT: web add new dependency, update web pages/demo/ComponentTest

--- web/src/pages/demo/ComponentTest/index.tsx
+++ web/src/pages/demo/ComponentTest/index.tsx
@@ -3,57 +3,78 @@
  */
 
 import React, {useState} from 'react'
-import {Divider, Space} from "antd"
-
-import {EditableTagPanel, SearchableTagsPanel} from "@/components/Gallery/Tag"
-import {Editor} from "@/components/Editor"
+import {Tag} from "antd"
+import {DragDropContext, Droppable, Draggable, DropResult} from 'react-beautiful-dnd'
 
 
-interface Data {
-  name: string
-  description: string
+interface Item {
+  id: string
+  content: React.ReactElement // custom component
 }
 
-const defaultData: Data[] = [
-  {
-    name: "Scala",
-    description: "FP + OOP"
-  }
-]
+
+const getItems = (count: number): Item[] =>
+  Array.from({length: count}, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: <Tag>{`item ${k}`}</Tag>
+  }))
+
+const reorder = (list: Item[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
+
+const grid = 8
+
+const getListStyle = (isDraggingOver: boolean) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  display: 'flex',
+  padding: grid,
+  overflow: 'auto',
+})
+
 
 export default () => {
 
-  const [editable, setEditable] = useState<boolean>(false)
-  const [data, setData] = useState<Data[]>(defaultData)
+  const [items, setItems] = useState(getItems(6))
 
-  const elementOnCreate = (v: Data) =>
-    setData([...data, v])
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) return
 
-  const elementOnModify = (v: Data) =>
-    console.log(v)
-
-  const elementOnRemove = (v: string) =>
-    setData(data.filter(d => d.name !== v))
+    const newItems = reorder(items, result.source.index, result.destination.index)
+    setItems(newItems)
+  }
 
   return (
-    <Space direction="vertical">
-      <Editor onChange={setEditable}/>
-      <EditableTagPanel
-        data={data}
-        editable={editable}
-        elementOnCreate={elementOnCreate}
-        elementOnRemove={elementOnRemove}
-        elementOnClick={elementOnModify}
-      />
-
-      <Divider/>
-
-      <SearchableTagsPanel
-        searchable={editable}
-        data={data}
-        elementOnSearch={d => console.log(d)}
-      />
-    </Space>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable" direction="horizontal">
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+            {...provided.droppableProps}
+          >
+            {items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {item.content}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
 
